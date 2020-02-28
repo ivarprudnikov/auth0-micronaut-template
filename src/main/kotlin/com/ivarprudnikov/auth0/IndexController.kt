429bb343959cd41be498b2e9599fa9ef1a845731
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-02-28 23:52:55 +0000

    Enable Auth0 JWT tokens and protect dummy endpoint

--- src/main/kotlin/com/ivarprudnikov/auth0/IndexController.kt
+++ src/main/kotlin/com/ivarprudnikov/auth0/IndexController.kt
@@ -2,10 +2,14 @@ package com.ivarprudnikov.auth0
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Produces
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
+import io.micronaut.security.rules.SecurityRule
+import javax.annotation.security.PermitAll
 
+@PermitAll
 @Controller("/")
 class IndexController {
 
@@ -13,5 +17,12 @@ class IndexController {
     @Produces(MediaType.TEXT_PLAIN)
     fun index(): String {
         return "UP"
+    }
+
+    @Get("/me")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    fun me(authentication: Authentication): Authentication {
+        return authentication
     }
 }
