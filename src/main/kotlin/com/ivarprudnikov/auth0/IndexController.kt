5321c6ef980dfeaf37a8a0a9571be55fd7dd56b1
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-03-02 00:47:09 +0000

    a bit of logging in index controller

--- src/main/kotlin/com/ivarprudnikov/auth0/IndexController.kt
+++ src/main/kotlin/com/ivarprudnikov/auth0/IndexController.kt
@@ -7,22 +7,28 @@ import io.micronaut.http.annotation.Produces
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import javax.annotation.security.PermitAll
 
 @PermitAll
 @Controller("/")
 class IndexController {
 
+    var logger: Logger = LoggerFactory.getLogger(IndexController::class.java)
+
     @Get("/")
     @Produces(MediaType.TEXT_PLAIN)
     fun index(): String {
+        logger.trace("status check")
         return "UP"
     }
 
     @Get("/me")
     @Produces(MediaType.APPLICATION_JSON)
     @Secured(SecurityRule.IS_AUTHENTICATED)
-    fun me(authentication: Authentication): Authentication {
+    fun me(authentication: Authentication?): Authentication? {
+        logger.trace("authentication check")
         return authentication
     }
 }
