3fea12e0536f6dc13ba12e74c412259f2f165727
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-03-04 18:44:22 +0000

    micronaut run() instead of build().start()

--- src/main/kotlin/com/ivarprudnikov/auth0/Application.kt
+++ src/main/kotlin/com/ivarprudnikov/auth0/Application.kt
@@ -6,9 +6,6 @@ object Application {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        Micronaut.build()
-                .packages("com.ivarprudnikov.auth0")
-                .mainClass(Application.javaClass)
-                .start()
+        Micronaut.run(Application.javaClass)
     }
 }
