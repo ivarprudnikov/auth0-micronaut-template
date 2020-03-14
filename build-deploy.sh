79b3ee4e2e0291f3cd9bbd708009f974837f3567
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-03-14 00:03:22 +0000

    remove graal branch deployment and rename env var that create S3 bucket and CF stack

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 
+NAME="${LAMBDA_APP_NAME:?Unique app name required}"
+
 ./build-function-zip.sh
 
-BUCKET_NAME="${LAMBDA_S3_BUCKET_NAME:?S3 bucket name required}"
-
 # Create S3 bucket used for deployment if one does not yet exist
-if aws s3 ls s3://${BUCKET_NAME} 2>&1 | grep -q 'NoSuchBucket'; then
-    aws s3 mb s3://${BUCKET_NAME}
+if aws s3 ls s3://${NAME} 2>&1 | grep -q 'NoSuchBucket'; then
+    aws s3 mb s3://${NAME}
 fi
 
-sam package --output-template-file packaged.yaml --s3-bucket ${BUCKET_NAME}
+sam package --output-template-file packaged.yaml --s3-bucket ${NAME}
 sam deploy --template-file packaged.yaml \
-        --stack-name ${BUCKET_NAME} \
+        --stack-name ${NAME} \
         --capabilities CAPABILITY_IAM
