bac33c70554b1b4377d60f2c65d563207dc3a9e5
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-05-01 15:57:25 +0100

    test options request

--- src/test/kotlin/com/ivarprudnikov/auth0/IndexControllerTest.kt
+++ src/test/kotlin/com/ivarprudnikov/auth0/IndexControllerTest.kt
@@ -48,6 +48,27 @@ class IndexControllerTest(@Client("/") client: RxHttpClient, tokenValidator: Tok
 
         verify { mock.validateToken("foobar") }
     }
+
+    "me path OPTIONS request responds" {
+        val response: HttpResponse<String> = client.toBlocking().exchange(
+                HttpRequest.OPTIONS<String>("/me").headers(mutableMapOf<CharSequence, CharSequence>(
+                        "Host" to "te60oj36jd.execute-api.eu-west-1.amazonaws.com",
+                        "User-Agent" to "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0",
+                        "Accept" to "*/*",
+                        "Accept-Language" to "en-US,en;q=0.5",
+                        "Accept-Encoding" to "gzip, deflate, br",
+                        "Access-Control-Request-Method" to "GET",
+                        "Access-Control-Request-Headers" to "authorization",
+                        "Referer" to "https://ivarprudnikov.github.io/react-auth0-template/profile",
+                        "Origin" to "https://ivarprudnikov.github.io",
+                        "Connection" to "keep-alive",
+                        "Pragma" to "no-cache",
+                        "Cache-Control" to "no-cache",
+                        "TE" to "Trailers"
+                )), String::class.java)
+        response.status shouldBe HttpStatus.OK
+        response.body() shouldBe null
+    }
 }) {
     @MockBean(TokenValidator::class)
     fun tokenValidator(): TokenValidator {
